ad612b66fdf517f06c66bef87e8f2b0f4de5eebf
Tienyu Yang <53754724+lingrottin@users.noreply.github.com>
2024-10-16 22:54:16 +0800

    loyalfier_test improvement

--- loyalfier_test/src/main.rs
+++ loyalfier_test/src/main.rs
@@ -6,10 +6,18 @@ use photon_rs::{native::save_image, PhotonImage};
 use rand::Rng;
 use std::{collections::HashMap, rc::Rc};
 fn main() {
-    let path = String::from(".\\images\\");
+    // Configuration field
+    const SAMPLES: usize = 20;
+    const COLUMNS: usize = 4;
+    const ROWS: usize = 12;
+    const PAGES: usize = 5;
+    let paper_size = PaperSize::B5.pixels();
+    let in_path = String::from(".\\images_\\");
+    let out_path = String::from(".\\output\\");
+
     let mut images: Vec<Rc<PhotonImage>> = vec![];
-    for i in 1..=12 {
-        let img = photon_rs::native::open_image(&(path.clone() + &i.to_string() + ".png"));
+    for i in 1..=SAMPLES {
+        let img = photon_rs::native::open_image(&(in_path.clone() + &i.to_string() + ".png"));
         match img {
             Ok(x) => {
                 images.push(Rc::new(x));
@@ -21,7 +29,7 @@ fn main() {
     }
     let mut samples: Vec<Sample> = vec![];
     let mut map: HashMap<Sample, Rc<PhotonImage>> = HashMap::new();
-    for i in 0..12 {
+    for i in 0..SAMPLES {
         let new_smp = Sample {
             id: i,
             dummy: false,
@@ -29,43 +37,31 @@ fn main() {
         samples.push(new_smp);
         map.insert(new_smp, images[i].clone());
     }
-    let table = make_table(samples, 1, 10, 8).unwrap();
+    let table = make_table(samples, PAGES, ROWS, COLUMNS).unwrap();
     let mut papers: Vec<Paper> = vec![];
-    for i in 0..1 {
-        papers.push(
-            Paper::make(
-                map.clone(),
-                table.clone(),
-                i,
-                PaperSize::pixels(&PaperSize::A4),
-            )
-            .unwrap(),
-        );
+    for i in 0..PAGES {
+        papers.push(Paper::make(map.clone(), table.clone(), i, paper_size).unwrap());
     }
     let mut rng = rand::thread_rng();
-    // for index in 0..3 {
-    let index = 0;
-    let i = &papers[index];
-    /*let obf = match rng.gen_range(0..=1) {
-        0 => PaperObfuscation::Upward,
-        1 => PaperObfuscation::Downward,
-        _ => panic!("Should never happen"),
-    };*/
-    let obf = PaperObfuscation::Upward;
-    //let row = rng.gen_range(1..=3);
-    let row = 3;
-    i.obfuscate(obf, row, 30.0);
-    //i.alter();
-    let ultimate = i.make_image();
-    let x = save_image(
-        ultimate,
-        &(String::from("./") + &index.to_string() + ".png"),
-    );
-    match x {
-        Ok(_) => {}
-        Err(x) => {
-            println!("{:?}", x);
+    for index in 0..PAGES {
+        let i = &papers[index];
+        let obf = match rng.gen_range(0..=1) {
+            0 => PaperObfuscation::Upward,
+            1 => PaperObfuscation::Downward,
+            _ => panic!("Should never happen"),
+        };
+        //let obf = PaperObfuscation::Upward;
+        let row = rng.gen_range(1..=ROWS);
+        //let row = 3;
+        i.obfuscate(obf, row, 30.0);
+        i.alter();
+        let ultimate = i.make_image();
+        let x = save_image(ultimate, &(out_path.clone() + &index.to_string() + ".png"));
+        match x {
+            Ok(_) => {}
+            Err(x) => {
+                println!("{:?}", x);
+            }
         }
     }
-    //}
 }
